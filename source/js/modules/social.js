9e99fe2a19507131a9219da95607263888618d3b
Maksim Glinka <xnagpa@outlook.com>
2022-09-01 16:49:23 +0400

    Добавьте плавную смену состояний иконки «Поделиться» и иконок социальных сетей. Обратите внимание, что иконки должны появляться по очереди, а пропадать — все вместе.

--- source/js/modules/social.js
+++ source/js/modules/social.js
@@ -1,9 +1,16 @@
 export default () => {
   let socialBlock = document.querySelector(`.js-social-block`);
+  let socialBlockListItems = Array.from(document.querySelectorAll(`.social-block__link`));
   socialBlock.addEventListener(`mouseover`, function () {
     socialBlock.classList.add(`social-block--active`);
+    let delay = 500;
+    socialBlockListItems.forEach((item) => {
+      setTimeout(() => item.classList.add(`social-block__link--fadein`), delay);
+      delay += 500;
+    });
   });
   socialBlock.addEventListener(`mouseleave`, function () {
     socialBlock.classList.remove(`social-block--active`);
+    socialBlockListItems.forEach((item) => item.classList.remove(`social-block__link--fadein`));
   });
 };
